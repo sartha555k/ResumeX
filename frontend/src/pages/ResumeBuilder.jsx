7e28496750a6015d029198dfda8e8851edd817e9
sartha555k <sartha555k@gmail.com>
2025-11-30 18:54:50 +0530

    feat: implement PersonalInfoForm component and integrate it into ResumeBuilder

--- frontend/src/pages/ResumeBuilder.jsx
+++ frontend/src/pages/ResumeBuilder.jsx
@@ -1,9 +1,146 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { Link, useParams } from "react-router-dom";
+import { dummyResumeData } from "../assets/assets";
+import PersonalInfoForm from "../components/PersonalInfoForm";
+import {
+  ArrowLeft,
+  ArrowLeftIcon,
+  Briefcase,
+  ChevronLeft,
+  ChevronRight,
+  FileText,
+  FolderIcon,
+  GraduationCap,
+  Sparkle,
+  User,
+} from "lucide-react";
+
 
 const ResumeBuilder = () => {
-  return (
-    <div>ResumeBuilder</div>
-  )
-}
+  const { resumeId } = useParams();
+  const [resumeData, setResumeData] = useState({
+    _id: "",
+    title: "",
+    personal_info: {},
+    professional_summary: "",
+    experience: [],
+    education: [],
+    project: [],
+    skills: [],
+    template: "classic",
+    accent_color: "#3B82F6",
+    public: false,
+  });
+  const loadExistingResumeData = async () => {
+    const resume = dummyResumeData.find((resume) => resume._id === resumeId);
+    if (resume) {
+      setResumeData(resume);
+      document.title = resume.title;
+    }
+  };
+  const [activeSectionIndex, setActiveSectionIndex] = useState(0);
+  const [removeBackground, setRemoveBackground] = useState(false);
+  const section = [
+    { id: "personal", name: "Personal Info", icon: User },
+    { id: "summary", name: "Summary", icon: FileText },
+    { id: "exprience", name: "Experience", icon: Briefcase },
+    { id: "education", name: "Education", icon: GraduationCap },
+    { id: "project", name: "Project", icon: FolderIcon },
+    { id: "skills", name: "Skills", icon: Sparkle },
+  ];
 
-export default ResumeBuilder
+  const activeSection = section[activeSectionIndex];
+  useEffect(() => {
+    loadExistingResumeData();
+  });
+  return (
+    <div>
+      <div className="max-w-7xl mx-auto px-4 py-6">
+        <Link
+          to={"/app"}
+          className="inline-flex gap-2 items-center text-slate-500
+        hover:text-slate-700 transition-all"
+        >
+          <ArrowLeftIcon className="size-4" />
+          Back to dashboard
+        </Link>
+      </div>
+
+      <div className="max-w-7xl mx-auto px-4 pb-8">
+        <div className="grid lg:grid-cols-12 gap-8">
+          {/* {left panel } */}
+          <div className="relative lg:col-span-5 rounded-lg overflow-hidden">
+            <div
+              className="bg-white rounded-lg shadow-sm border border-gray-300
+            p-6 pt-1"
+            >
+              <hr className="absolute top-0 left-0 right-0 border-2 border-gray-200" />
+              <hr
+                className="absolute top-0 left-0 h-1 bg-linear-to-r from-green-500
+              to-green-600 border-none transition-all duration-2000"
+                style={{
+                  width: `${
+                    (activeSectionIndex * 100) / (section.length - 1)
+                  }%`,
+                }}
+              />
+              {/* {section navigation} */}
+              <div
+                className="flex justify-between items-center mb-6 border-b
+              border-gray-200 py-1"
+              >
+                <div></div>
+                <div className="flex items-center">
+                  {activeSectionIndex != 0 && (
+                    <button
+                      onClick={() =>
+                        setActiveSectionIndex((prevIndex) =>
+                          Math.max(prevIndex - 1, 0)
+                        )
+                      }
+                      className="flex items-center gap-1 p-3 
+                    rounded-lg text-sm font-medium text-gray-600 
+                    hover:text-gray-500 transition-all"
+                      disabled={activeSectionIndex === 0}
+                    >
+                      <ChevronLeft className="size-4" /> Previous
+                    </button>
+                  )}
+                  <button
+                    onClick={() =>
+                      setActiveSectionIndex((prevIndex) =>
+                        Math.min(prevIndex + 1, section.length - 1)
+                      )
+                    }
+                    className={`flex items-center gap-1 p-3 rounded-lg text-sm font-medium
+                      text-gray-600 hover:text-gray-500 transition-all ${
+                        activeSectionIndex === section.length - 1 &&
+                        "opacity-50"
+                      }`}
+                    disabled={activeSectionIndex === section.length - 1}
+                  >
+                    <ChevronRight className="size-4" /> Next
+                  </button>
+                </div>
+              </div>
+
+              {/* {from content } */}
+              <div className="space-y-6">
+                {activeSection.id === "personal" && (
+                  <PersonalInfoForm data={resumeData.personal_info} 
+                  onChange={(data)=>setResumeData(prev => ({...prev , personal_info:data}))}
+                  removeBackground={removeBackground}setRemoveBackground={setRemoveBackground}/>
+                )}
+
+              </div>
+            </div>
+          </div>
+          {/* {right panel } */}
+          <div></div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ResumeBuilder;
