a32c97b8a9e6c47e510995550297896742e384fa
sartha555k <sartha555k@gmail.com>
2025-12-01 12:52:19 +0530

    feat: add user and resume routes with authentication; implement resume controller and middleware for enhanced functionality

--- Backend/Server.js
+++ Backend/Server.js
@@ -2,6 +2,8 @@ import express from "express";
 import cors from "cors";
 import "dotenv/config";
 import connectDB from "./configs/db.js";
+import userRouter from "./routes/userRoutes.js";
+import resumeRouter from "./routes/resumeRoutes.js";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -11,11 +13,13 @@ await connectDB()
 app.use(express.json())
 app.use(cors())
 
-app.get("/", (req, res)=>{
+app.get("/", (req, res) => {
     res.send("API is running...");
 });
+app.use('/api/users', userRouter)
+app.use('/api/resumes', resumeRouter)
 
-app.listen(PORT, ()=>{
+app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
-    
+
 });
